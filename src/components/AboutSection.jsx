2d2d3d7a7dfa4c521682e2b202e9df22d306e2dc
perrylzx <46482518+perrylzx@users.noreply.github.com>
2022-06-25 03:02:33 +0800

    Update about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,26 +3,39 @@ import styled from 'styled-components';
 
 const AboutSectionContainer = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
   height: 100vh;
   width: 100%;
-  padding: 0 200px;
+  padding: 46px 0;
+  line-height: 1.3;
 `;
 
-const SearchBar = styled.div``;
+const Name = styled.h1`
+  margin-bottom: 40px;
+`;
 
-const FullServices = styled.h1`
-  text-align: center;
+const Description = styled.h2`
+  max-width: 70%;
+  font-size: 15px;
+  font-weight: 400;
+`;
+
+const AboutContainer = styled.div`
+  margin: auto;
+  max-width: 50%;
 `;
 
 function AboutSection(props) {
   return (
     <AboutSectionContainer {...props}>
-      <FullServices>
-        FULL SERVICES FROM A FULL-STACK DEVELOPER OF AN ESTABLISHED SERIES C START-UP
-      </FullServices>
-      <SearchBar />
+      <AboutContainer>
+        <Name>Hi, my name is Perry Lim</Name>
+        <Description>
+          I&apos;m a full-stack software engineer based in Singapore specializing in React and
+          Express. On my free time I build things that automate the boring parts of everyday life.
+          Check my projects out below.
+        </Description>
+      </AboutContainer>
     </AboutSectionContainer>
   );
 }
