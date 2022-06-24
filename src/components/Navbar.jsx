16eecf0b9705d414437df8a1ff4434842788e533
perrylzx <46482518+perrylzx@users.noreply.github.com>
2022-06-25 03:11:25 +0800

    Add logo to navbar that scrolls window to top of page - General styling changes - Change navbar items to align to the end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,30 @@ import { SectionIds } from '../constants';
 
 const Menu = styled(BaseMenu)`
   display: flex;
-  justify-content: center;
+  justify-content: end;
   background-color: #1d263b;
+  span {
+    height: 100%;
+  }
+  a {
+    height: 100%;
+  }
 `;
-console.log('haha');
+
+const LogoName = styled.a.attrs({ href: `#${SectionIds.MAIN}` })`
+  background: linear-gradient(-70deg, #a2facf 0%, #64acff 100%);
+  position: absolute;
+  top: 6px;
+  left: 20px;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 20px;
+`;
 
 function Navbar() {
   return (
     <Affix>
+      <LogoName>SMUGPUG</LogoName>
       <Menu mode="horizontal">
         <Menu.Item>
           <a href={`#${SectionIds.ABOUT}`}>Perry</a>
